3e5ccee36d0332a6dc992762d266b1303b3bed7e
tet-git <tet.github@tet.orz.hm>
2017-05-23 23:29:21 +0900

    Create ja.ts

---
+++ svplight/ja.ts
@@ -0,0 +1,386 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>LEDConfiguration</name>
+    <message>
+        <location filename="ledconfiguration.ui" line="14"/>
+        <source>LED hardware</source>
+        <translation type="unfinished">LED機器</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="55"/>
+        <source>Hardware configuration</source>
+        <translation type="unfinished">ハードウェア構成</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="86"/>
+        <source>LED positions adjustment</source>
+        <translation type="unfinished">LED位置調整</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="189"/>
+        <source>Lightpack USB</source>
+        <translation type="unfinished">Lightpack USB</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="220"/>
+        <source></source>
+        <translation type="unfinished">Prismatik API</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="251"/>
+        <source></source>
+        <translation type="unfinished">Paintpack DMX</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="307"/>
+        <source>Screen with LEDs attached</source>
+        <translation type="unfinished">LED取付済の画面</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="389"/>
+        <source>Import configuration</source>
+        <translation type="unfinished">設定ファイル読み込み</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="403"/>
+        <source>No compatible lighting software found.</source>
+        <translation type="unfinished">互換性のある照明ソフトウェアが見つかりませんでした。</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="427"/>
+        <source>From &lt;a href=#&gt;AmbiBox&lt;/a&gt; profile</source>
+        <translation type="unfinished">&lt;a href=#&gt;AmbiBox&lt;/a&gt;用プロファイル</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="459"/>
+        <location filename="ledconfiguration.ui" line="513"/>
+        <source>&lt;a href=#&gt;Import&lt;/a&gt;</source>
+        <translation type="unfinished">&lt;a href=#&gt;インポート&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="484"/>
+        <source>From &lt;a href=#&gt;Prismatik&lt;/a&gt; profile</source>
+        <translation type="unfinished">&lt;a href=#&gt;Prismatik&lt;/a&gt;用プロファイル</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="557"/>
+        <source>Number of light zones</source>
+        <translation type="unfinished">ライトゾーンの数</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="584"/>
+        <source> zones</source>
+        <translation type="unfinished">ゾーン</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="609"/>
+        <source>&lt;a href=#&gt;Click to set&lt;/a&gt;</source>
+        <translation type="unfinished">&lt;a href=#&gt;リックして設定&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="668"/>
+        <source>Additional settings</source>
+        <translation type="unfinished">その他の設定</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="682"/>
+        <source>Color channels order</source>
+        <translation type="unfinished">カラーチャンネル指定</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="714"/>
+        <source>R - G - B (default)</source>
+        <translation type="unfinished">Red - Green - Blue (規定)</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="719"/>
+        <source>G - R - B</source>
+        <translation type="unfinished">G - R - B</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="724"/>
+        <source>R - B - G</source>
+        <translation type="unfinished">R - B - G</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="729"/>
+        <source>G - B - R</source>
+        <translation type="unfinished">G - B - R</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="734"/>
+        <source>B - G - R</source>
+        <translation type="unfinished">B - G - R</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="739"/>
+        <source>B - R - G</source>
+        <translation type="unfinished">B - R - G</translation>
+    </message>
+    <message>
+        <location filename="ledconfiguration.ui" line="929"/>
+        <source>Adjust brightness and colors...</source>
+        <translation type="unfinished">輝度と色の調整...</translation>
+    </message>
+</context>
+<context>
+    <name>LEDConfigurationWnd</name>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="128"/>
+        <source>2 zones</source>
+        <translation type="unfinished">２ゾーン</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="129"/>
+        <source>4 zones</source>
+        <translation type="unfinished">４ゾーン</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="130"/>
+        <source>10 zones</source>
+        <translation type="unfinished">10ゾーン</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="131"/>
+        <source>20 zones</source>
+        <translation type="unfinished">20ゾーン</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="132"/>
+        <source>30 zones</source>
+        <translation type="unfinished">30ゾーン</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="133"/>
+        <source>60 zones</source>
+        <translation type="unfinished">60ゾーン</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="134"/>
+        <source>Custom...</source>
+        <translation type="unfinished">カスタム</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="238"/>
+        <source>Toggle fullscreen mode</source>
+        <translation type="unfinished">フルスクリーンモードに切り替え</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="243"/>
+        <source>Turn lights on or off</source>
+        <translation type="unfinished">照明オン／オフ</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="248"/>
+        <source>Turn runnign lights effect on or off</source>
+        <translation type="unfinished">イルミネーション オン／オフ</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="318"/>
+        <source>Reset configuration</source>
+        <translation type="unfinished">設定をリセット</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="318"/>
+        <source>Current lights configuration will be lost! Is it OK to continue?</source>
+        <translation type="unfinished">現在のライトの設定が失われます！続行しますか？</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="693"/>
+        <source>Prismatik profile</source>
+        <translation type="unfinished">Prismatik用プロファイル</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="693"/>
+        <source>Error importing profile from Prismatik.</source>
+        <translation type="unfinished">Prismatikからプロファイルをインポート中にエラーが発生しました。</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="726"/>
+        <source>Prismatik</source>
+        <translation type="unfinished">Prismatik</translation>
+    </message>
+    <message>
+        <location filename="ledconfigurationwnd.cpp" line="726"/>
+        <source>For the better experience please open Prismatik settings window and set &quot;Mode - Scene luminosity threshold&quot; to 1 (one).</source>
+        <translation type="unfinished">更に調整するには、Prismatik設定ウィンドウを開き、"Mode - Scene luminosity threshold"を1に設定してください。</translation>
+    </message>
+</context>
+<context>
+    <name>LEDControl</name>
+    <message>
+        <location filename="ledcontrol.ui" line="14"/>
+        <source>LED lighting</source>
+        <translation type="unfinished">LED lighting</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="76"/>
+        <source>SVPlight - LED lighting</source>
+        <translation type="unfinished">SVPlight - LED lighting</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="108"/>
+        <source>Reset all options to default values</source>
+        <translation type="unfinished">全設定をリセット</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="111"/>
+        <source>Reset</source>
+        <translation type="unfinished">リセット</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="169"/>
+        <source>Brightness</source>
+        <translation type="unfinished">輝度</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="200"/>
+        <location filename="ledcontrol.ui" line="275"/>
+        <location filename="ledcontrol.ui" line="335"/>
+        <source>MIN</source>
+        <translation type="unfinished">最小値</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="210"/>
+        <location filename="ledcontrol.ui" line="285"/>
+        <location filename="ledcontrol.ui" line="345"/>
+        <location filename="ledcontrol.ui" line="450"/>
+        <source>MAX</source>
+        <translation type="unfinished">最大値</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="229"/>
+        <source>Gamma</source>
+        <translation type="unfinished">ガンマ値</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="248"/>
+        <source>Make dark scenes brighter</source>
+        <translation type="unfinished">暗いシーンをより明るく</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="304"/>
+        <source>Saturation</source>
+        <translation type="unfinished">彩度</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="394"/>
+        <source>Smooth motion</source>
+        <translation type="unfinished">スムース モーション</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="413"/>
+        <source>Smooth transitions between colors</source>
+        <translation type="unfinished">カラー間のスムーズな遷移</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="440"/>
+        <location filename="ledcontrol.ui" line="570"/>
+        <source>OFF</source>
+        <translation type="unfinished">オフ</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="469"/>
+        <source>Color correction</source>
+        <translation type="unfinished">色補正</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="491"/>
+        <source>Click to choose wall color</source>
+        <translation type="unfinished">クリックして地の色を選択</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="518"/>
+        <source>Delay</source>
+        <translation type="unfinished">延滞</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="537"/>
+        <source>Presentation delay, in ms</source>
+        <translation type="unfinished">プレゼンテーションの遅延(ミリ秒単位)</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="608"/>
+        <source>+1 s</source>
+        <translation type="unfinished">+1 秒</translation>
+    </message>
+    <message>
+        <location filename="ledcontrol.ui" line="671"/>
+        <source>Setup LED lighting hardware...</source>
+        <translation type="unfinished"> LED lighting機器のセットアップ</translation>
+    </message>
+</context>
+<context>
+    <name>LEDControlWnd</name>
+    <message>
+        <location filename="ledcontrolwnd.cpp" line="75"/>
+        <source>No engine</source>
+        <translation type="unfinished">機能なし</translation>
+    </message>
+    <message>
+        <location filename="ledcontrolwnd.cpp" line="84"/>
+        <source>OK</source>
+        <translation type="unfinished">成功</translation>
+    </message>
+    <message>
+        <location filename="ledcontrolwnd.cpp" line="84"/>
+        <source>FAIL</source>
+        <translation type="unfinished">失敗</translation>
+    </message>
+    <message>
+        <location filename="ledcontrolwnd.cpp" line="90"/>
+        <source>Paintpack isn&apos;t connected OR K8062d library not found.</source>
+        <translation type="unfinished">Paintpack が未接続か、または K8062d ライブラリが見つかりません。</translation>
+    </message>
+    <message>
+        <location filename="ledcontrolwnd.cpp" line="95"/>
+        <source>Lighting hardware isn&apos;t connected OR is blocked by some other app.</source>
+        <translation type="unfinished">照明機器が接続されていないか、他のアプリケーションによってブロックされています。</translation>
+    </message>
+    <message>
+        <location filename="ledcontrolwnd.cpp" line="99"/>
+        <source>Playing video...</source>
+        <translation type="unfinished">動画再生中...</translation>
+    </message>
+    <message>
+        <location filename="ledcontrolwnd.cpp" line="125"/>
+        <source>%1%2 ms</source>
+        <translation type="unfinished">%1%2 ms</translation>
+    </message>
+    <message>
+        <location filename="ledcontrolwnd.cpp" line="141"/>
+        <source>Choose a wall color</source>
+        <translation type="unfinished">地の色を選択</translation>
+    </message>
+</context>
+<context>
+    <name>LightPlugin</name>
+    <message>
+        <location filename="lightplugin.cpp" line="69"/>
+        <source>Toggle LED lighting</source>
+        <translation type="unfinished">LED lightingを切り替え</translation>
+    </message>
+    <message>
+        <location filename="lightplugin.cpp" line="74"/>
+        <source>LED lighting</source>
+        <translation>LED lighting</translation>
+    </message>
+    <message>
+        <location filename="lightplugin.cpp" line="75"/>
+        <source>Turn on</source>
+        <translation type="unfinished">点灯</translation>
+    </message>
+    <message>
+        <location filename="lightplugin.cpp" line="84"/>
+        <source>Adjust brightness and colors</source>
+        <translation type="unfinished">輝度と色の調整</translation>
+    </message>
+    <message>
+        <location filename="lightplugin.cpp" line="86"/>
+        <source>Setup hardware...</source>
+        <translation type="unfinished">機器のセットアップ...</translation>
+    </message>
+</context>
+</TS>
